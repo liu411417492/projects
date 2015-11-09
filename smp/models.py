fab97bf1a4960ca86f2a415bdd0f9cd9787adc66
unknown <liulongdu@163.com>
2015-11-09 21:27:20 +0800

    B3

--- smp/models.py
+++ smp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-        
+#change B3 here
 class Auther(models.Model):
     name = models.CharField(max_length= 30)
     institution = models.CharField(max_length= 30)
--- smp/tests.py
+++ smp/tests.py
@@ -1,3 +1,3 @@
 from django.test import TestCase
-
+#change B3 here
 # Create your tests here.
